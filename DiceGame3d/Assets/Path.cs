b063a5b7d83308e986499a06cd1c131a1708287d
nemesisverse <129728622+nemesisverse@users.noreply.github.com>
2024-06-03 04:49:10 +0530

    update 2

---
+++ DiceGame3d/Assets/Path.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Path : MonoBehaviour
+{
+    Transform[] ChildNodes;
+
+    public List<Transform> ChildNodeList = new List<Transform>();
+
+    void Start()
+    {
+        FillNodes();
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.blue;
+        FillNodes();
+
+        for (int i = 0; i < ChildNodeList.Count; i++)
+        {
+            Vector3 pos = ChildNodeList[i].position;
+            if (i > 0)
+            {
+                Vector3 prev = ChildNodeList[i - 1].position;
+                Gizmos.DrawLine(pos, prev);
+            }
+        }
+    }
+
+    void FillNodes()
+    {
+        ChildNodeList.Clear();
+        ChildNodes = GetComponentsInChildren<Transform>();
+
+        foreach(Transform Child in ChildNodes)
+        {
+            if(Child != this.transform)
+            {
+                ChildNodeList.Add(Child);
+            }
+        }
+    }
+
+    
+    
+}
